3eac00069b91b30ec799a8b1a0abae734d8e48be
Gustavo <gugurock50@gmail.com>
2020-10-22 17:31:02 -0300

    Create cutRod.cpp

---
+++ projeto-e-analise-de-algoritmos/cutRod.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <limits.h>
+
+int max(int a, int b) {
+    return (a > b)? a : b;
+}
+
+int cutRod(int vetor[], int n)
+{
+	if (n <= 0)
+		return 0;
+
+	int maxVal = INT_MIN;
+
+	for (int i = 0; i<n; i++)
+		maxVal = max(maxVal, vetor[i] + cutRod(vetor, n-i-1));
+
+	return maxVal;
+}
+
+int main()
+{
+	int vetor[] = { 1, 5, 8, 9, 10, 17, 17, 20 };
+	int size = sizeof(vetor)/sizeof(vetor[0]);
+	std::cout << "Maximum value: " << cutRod(vetor, size);
+	return 0;
+}
