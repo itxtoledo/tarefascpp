62e88097b84dfcd45e95db27b2f4f458751d7def
Gustavo <gugurock50@gmail.com>
2020-10-26 11:53:04 -0300

    Update bottomUpCutRod.cpp

--- projeto-e-analise-de-algoritmos/bottomUpCutRod.cpp
+++ projeto-e-analise-de-algoritmos/bottomUpCutRod.cpp
@@ -8,14 +8,14 @@ int max(int a, int b) {
 int bottomUpCutRod(int p[],int n){
     int r[n+1];
     int maxVal;
-    
+
     r[0] = 0;
 
     for (int j = 1; j <= n; j++)
     {
         maxVal = INT_MIN;
         for (int i = 0; i < j; i++)
-            maxVal = max(q,p[i]+r[j-i-1]);
+            maxVal = max(maxVal,p[i]+r[j-i-1]);
         r[j] = maxVal;
     }
 
